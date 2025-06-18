1a333caacef4a56a0c5fb80223555deb5ed8d322
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2025-06-18 12:40:47 +0200

    Ajout de logs pour afficher les variables d'environnement BACKEND_URL, ANALYSER_URL et PGSQL_PORT dans le formulaire d'inscription.

--- apps/frontend/components/sign-up/sign-up-form.tsx
+++ apps/frontend/components/sign-up/sign-up-form.tsx
@@ -52,6 +52,10 @@ export default function SignUpForm() {
 
   const submitRegistration = async () => {
     console.log(process.env.NEXT_PUBLIC_BACKEND_URL);
+    console.log(process.env.NEXT_PUBLIC_ANALYSER_URL);
+    console.log(process.env.PGSQL_PORT);
+    console.log(process.env.BACK_URL);
+
     console.log("submit");
     setError("");
     setSuccess("");
