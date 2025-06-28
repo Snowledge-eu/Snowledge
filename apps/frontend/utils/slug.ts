47b0fdf24b67bac0f25f46810d8f7ddab2dec154
Alexandre-Pascal <alexandre.pascal.ep@gmail.com>
2025-06-28 18:40:36 +0200

    Suppression des messages de débogage liés aux variables d'environnement dans les fonctions de chargement et d'appel de l'API OVH, et ajout d'une fonctionnalité pour remplacer '&' par 'and' dans la fonction de génération de slug.

--- apps/frontend/utils/slug.ts
+++ apps/frontend/utils/slug.ts
@@ -1,6 +1,7 @@
 export function toSlug(name: string): string {
   return name
     .normalize("NFD") // retire les accents
+    .replace(/&/g, "and") // remplace les & par 'and'
     .replace(/\p{Diacritic}/gu, "")
     .replace(/_/g, "") // supprime les underscores
     .replace(/[^a-zA-Z0-9]+/g, "-") // remplace tout ce qui n'est pas lettre ou chiffre par un tiret
